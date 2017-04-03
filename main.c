a4594c3aa349fd6bfa0be39438e204787228409b
Martyna Markiewicz <martyna.markiewicz01@gmail.com>
2017-04-03 17:12:17 +0200

    add ws2812b library

---
+++ main.c
@@ -0,0 +1,26 @@
+#include "stm32f4xx_conf.h"
+#include "stm32f4xx_gpio.h"
+#include "stm32f4xx_rcc.h"
+#include "stm32f4xx_dma.h"
+#include "ws2812b.h"
+
+int main(void)
+{
+	SystemInit();
+	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOA, ENABLE);
+	GPIO_InitTypeDef GPIO_InitStructure;
+
+	GPIO_InitStructure.GPIO_Pin =GPIO_Pin_1 ;
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_OUT;
+	GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
+	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_100MHz;
+	GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_NOPULL;
+	GPIO_Init(GPIOA, &GPIO_InitStructure);
+
+	WS2812B_Init();
+
+	while(1)
+	{
+		WS2812B_SetRGB(0,1,0,0, 250);
+	}
+}
