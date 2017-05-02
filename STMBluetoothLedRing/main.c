6bf5455eeae71ed5169535be36c4f36483fe0de7
Martyna Markiewicz <martyna.markiewicz01@gmail.com>
2017-05-02 12:16:40 +0200

    ustawienie jasności, sekwencji, kolorów - ring 2

--- STMBluetoothLedRing/main.c
+++ STMBluetoothLedRing/main.c
@@ -1,87 +1,64 @@
 #include "stm32f4xx_conf.h"
 #include <stm32f4xx.h>
-#include <stm32f4xx_rcc.h>
-#include <stm32f4xx_gpio.h>
-#include <stm32f4xx_tim.h>
-#include <stm32f4xx_dma.h>
-#include <stm32f4xx_usart.h>
-#include "stm32f4xx_exti.h"
-#include "stm32f4xx_adc.h"
-#include "stm32f4xx_syscfg.h"
-#include <misc.h>
-//#include "delay.h"
-#include "ws2812.h"
-#include "math.h"
-#include "stm32f4xx_usart.h"
-#include <string.h>
-#include <stdio.h>
-#include <stdbool.h>
-#include <ctype.h>
-#include <sekwencje.h>
-#include <colours.h>
 #include <clear.h>
+#include <colours.h>
+#include <ctype.h>
+#include "delay.h"
+#include "math.h"
+#include <misc.h>
+#include <sekwencje.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include "stm32f4xx_adc.h"
+#include <stm32f4xx_dma.h>
+#include "stm32f4xx_exti.h"
+#include <stm32f4xx_gpio.h>
+#include <stm32f4xx_rcc.h>
+#include "stm32f4xx_syscfg.h"
+#include <stm32f4xx_tim.h>
+#include <stm32f4xx_usart.h>
+#include <string.h>
 #include <time.h>
+#include "ws2812.h"
 
 #define false 0
 #define true 1
 
-int j=0;
-int x=0;
-bool skanuje,s,k,m;
+bool s,k,m;
+bool s1,k1,m1;
 
-volatile int delayFlag;
-void TIM3_IRQHandler(void)
-{
-         	if(TIM_GetITStatus(TIM3, TIM_IT_Update) != RESET)
-         	{
-         			GPIO_SetBits(GPIOD,GPIO_Pin_14|GPIO_Pin_15);
-         			delayFlag=1;
-         			diode[5].blue=255;
-       		}
-              	 //wyzerowanie flagi wyzwolonego przerwania
-               	TIM_ClearITPendingBit(TIM3, TIM_IT_Update);
-}
+uint8_t brightness,brightness2;
+uint8_t *pixels;
 
-void timer()
-{
-	RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM3, ENABLE);
+int x;
+bool x1;
 
-	TIM_TimeBaseInitTypeDef TIM_TimeBaseStructure;
-	TIM_TimeBaseStructure.TIM_Period = 8399;
-	TIM_TimeBaseStructure.TIM_Prescaler = 9999;
-	TIM_TimeBaseStructure.TIM_ClockDivision = TIM_CKD_DIV1;
-	TIM_TimeBaseStructure.TIM_CounterMode = TIM_CounterMode_Up ;
-	TIM_TimeBaseInit(TIM3, &TIM_TimeBaseStructure);
-	TIM_Cmd(TIM3, ENABLE);
-//	TIM_OCInitTypeDef TIM_OCInitStructure;
-//
-//	     TIM_OCInitStructure.TIM_OCMode = TIM_OCMode_Timing;
-//	     TIM_OCInitStructure.TIM_OutputState = TIM_OutputState_Enable;
-//	     TIM_OCInitStructure.TIM_Pulse = 83;   // T= 1us!
-//	     TIM_OCInitStructure.TIM_OCPolarity = TIM_OCPolarity_High;
-//
-//	   TIM_OC1Init(TIM1, &TIM_OCInitStructure);
-//
-//	   TIM_OC1PreloadConfig(TIM3,TIM_OCPreload_Enable);
-//	   TIM_ITConfig(TIM3, TIM_IT_CC1, ENABLE);
+int y;
+bool y2;
 
-	NVIC_PriorityGroupConfig(NVIC_PriorityGroup_1);
-	NVIC_InitTypeDef NVIC_InitStructure;
-	// numer przerwania
-	NVIC_InitStructure.NVIC_IRQChannel = TIM3_IRQn;
-	// priorytet g³ówny
-	NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 0x00;
-	// subpriorytet
-	NVIC_InitStructure.NVIC_IRQChannelSubPriority = 0x00;
-	// uruchom dany kana³
-	NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
-	// zapisz wype³nion¹ strukturê do rejestrów
-	NVIC_Init(&NVIC_InitStructure);
+int temp;
 
-	TIM_ClearITPendingBit(TIM3, TIM_IT_Update);
-	// zezwolenie na przerwania od przepe³nienia dla timera 3
-	TIM_ITConfig(TIM3, TIM_IT_Update, ENABLE);
-}
+int red_br,red_br2;
+int green_br,green_br2;
+int blue_br,blue_br2;
+
+int red_p,red_p2;
+int green_p,green_p2;
+int blue_p,blue_p2;
+
+volatile int zmienna_usart1,zmienna_usart12;
+volatile int zmienna_usart2,zmienna_usart22;
+volatile int zmienna_usart3,zmienna_usart32;
+volatile int zmienna_usart4,zmienna_usart42;
+volatile int zmienna_usart5,zmienna_usart52;
+volatile int zmienna_usart6,zmienna_usart62;
+volatile int turn_led,turn_led2;
+
+void rcc();
+void gpio();
+void usart();
+void setBrightness(uint8_t x);
+uint8_t getBrightness(void);
 
 void rcc()
 {
@@ -90,7 +67,6 @@ void rcc()
 	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOC, ENABLE);
 	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOD, ENABLE);
 	RCC_APB2PeriphClockCmd(RCC_APB2Periph_ADC1, ENABLE);
-	//RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM3, ENABLE );
 	RCC_APB1PeriphClockCmd(RCC_APB1Periph_USART3, ENABLE);
 }
 
@@ -148,71 +124,68 @@ void usart()
 	NVIC_EnableIRQ( USART3_IRQn );
 }
 
-uint8_t brightness;
-uint8_t *pixels;
-
-void setBrightness(uint8_t x) {
+void setBrightness(uint8_t x)
+{
   uint8_t newBrightness = x + 1;
-  if(newBrightness != brightness)
-  {
-    uint8_t  c,
+
+  // RING_1
+  	  if(newBrightness != brightness)
+  	  {
+  		  uint8_t  c,
             *ptr           = pixels,
              oldBrightness = brightness - 1;
-    uint16_t scale;
-    if(oldBrightness == 0) scale = 0;
-    else if(x == 255) scale = 65535 / oldBrightness;
-    else scale = (((uint16_t)newBrightness << 8) - 1) / oldBrightness;
-    for(uint16_t i=0; i<3; i++) {
-      c      = *ptr;
-      *ptr++ = (c * scale) >> 8;
-    }
-   brightness = newBrightness;
-   if(brightness==41)
-   {GPIO_SetBits(GPIOD,GPIO_Pin_14);}
-  }
+  		  uint16_t scale;
+  		  if(oldBrightness == 0) scale = 0;
+  		  else if(x == 255) scale = 65535 / oldBrightness;
+  		  else scale = (((uint16_t)newBrightness << 8) - 1) / oldBrightness;
+  		  for(uint16_t i=0; i<3; i++)
+  		  {
+  			  c      = *ptr;
+  			  *ptr++ = (c * scale) >> 8;
+  		  }
+  		  brightness = newBrightness;
+  	  }
+
+  // RING_2
+  	  if(newBrightness != brightness2)
+  	  {
+  		  uint8_t  c,
+             *ptr           = pixels,
+              oldBrightness = brightness2 - 1;
+  		  uint16_t scale;
+  		  if(oldBrightness == 0) scale = 0;
+  		  else if(x == 255) scale = 65535 / oldBrightness;
+  		  else scale = (((uint16_t)newBrightness << 8) - 1) / oldBrightness;
+  		  for(uint16_t i=0; i<3; i++)
+  		  {
+  			  c      = *ptr;
+  			  *ptr++ = (c * scale) >> 8;
+  		  }
+  		  brightness2 = newBrightness;
+  	  }
 }
 
-uint8_t getBrightness(void) {
+uint8_t getBrightness(void)
+{
   return brightness - 1;
 }
 
-int main(void)
-{
-	SystemInit();
-	rcc();
-	gpio();
-	usart();
-	//init_systick();	//delay
-	ws2812_init();	//LED + PWM
-	while(1)
-	{
-	}
-}
-
-int numerdiody;
-int red_p;
-int green_p;
-int blue_p;
-int c;
-
-int x;
-bool x1;
-
-int red_br;
-int green_br;
-int blue_br;
-int red_br1;
-int blue_br1;
-int green_br1;
-
-
 void USART3_IRQHandler(void)
 {
-		if(USART_GetITStatus(USART3, USART_IT_RXNE) != RESET)
+	if(USART_GetITStatus(USART3, USART_IT_RXNE) != RESET)
+	{
+		if(USART3->DR=='x'||temp==0)
 		{
-			//ODBIERANIE JASNOŒCI
+			//RING1
+			temp=0;
+			if(USART3->DR=='y')
+			{
+				temp=1;
+			}
+	//FUNKCJE WSZYSTKIE OD KOLORÓW itp. RING1
+
+	//ODBIERANIE JASNOŒCI
 			uint8_t c = USART3->DR;
-			//uint8_t c = USART3->DR;
 			//>>>>>>>>>>>>>>>>>>>>>>>
 			if(USART3->DR=='L')
 			{
@@ -286,8 +259,9 @@ void USART3_IRQHandler(void)
 				else
 				m=0;
 			}
-
+			// RGB - RING_1
 			rgb(red_p, green_p,blue_p);
+
 			if(brightness)
 			{
 				if(red_br==327)
@@ -311,7 +285,7 @@ void USART3_IRQHandler(void)
 					diode[i].blue=(blue_br* brightness) >> 8;
 				}
 			}
-			//do kolorow
+			// KOLORY - RING_1
 			if(USART3->DR == 'g')
 			{
 				red_br=135;
@@ -369,56 +343,54 @@ void USART3_IRQHandler(void)
 				rgb(red_br, green_br,blue_br);
 			}
 
-			//do sekwencji
+			// SEKWENCJE - RING_1
 			if(USART3->DR == 'a')
 			{
 				red_p=0;
 				green_p=0;
 				blue_p=0;
-
-				diode[0].blue=255;
-				diode[2].green=255;
+				zmienna_usart1=1;
 			}
 			if(USART3->DR == 'b')
 			{
 				red_p=0;
 				green_p=0;
 				blue_p=0;
-				s2();
+				zmienna_usart2=1;
 			}
 			if(USART3->DR == 'c')
 			{
 				red_p=0;
 				green_p=0;
 				blue_p=0;
-				clear();
-				s3();
+				zmienna_usart3=1;
 			}
 			if(USART3->DR == 'd')
 			{
 				red_p=0;
 				green_p=0;
 				blue_p=0;
-				s4();
+				zmienna_usart4=1;
 			}
 			if(USART3->DR == 'e')
 			{
 				red_p=0;
 				green_p=0;
 				blue_p=0;
-				s5();
+				zmienna_usart5=1;
 			}
 			if(USART3->DR == 'f')
 			{
 				red_p=0;
 				green_p=0;
 				blue_p=0;
-				s6();
+				zmienna_usart6=1;
 			}
-			//turn off
+			// TURN OFF
 			if(USART3->DR == 'O')
 			{
-				clear_app();
+				turn_led=1;
+				clear_app(0,8);
 				red_br=0;
 				green_br=0;
 				blue_br=0;
@@ -427,4 +399,370 @@ void USART3_IRQHandler(void)
 				blue_p=0;
 			}
 		}
+
+	////////////////////RING2/////////////////////////
+
+		if(USART3->DR=='y'||temp==1)
+		{
+			//RING2
+			temp=1;
+
+			if(USART3->DR=='x')
+			{
+				temp=0;
+			}
+
+	//FUNKCJE WSZYSTKIE OD KOLORÓW itp. / RING2
+	//ODBIERANIE JASNOŒCI
+			uint8_t c1 = USART3->DR;
+			//>>>>>>>>>>>>>>>>>>>>>>>
+			if(USART3->DR=='L')
+			{
+				y=0;
+				y2=1;
+			}
+			if(y2==1)
+			{
+				if(c1==0)
+				y=0;
+				if (c1 >= 1 && c1 <= 100&&c1!=82&&c1!=71&&c1!=66&&c1!=97&&c1!=98&&c1!=99&&c1!=100)
+				y = c1*10;
+				else
+				y2=0;
+				setBrightness(y);
+			}
+
+			if(USART3->DR == 'R')
+			{
+				brightness2=100;
+				red_p2 = 0;
+				s1 = 1;
+			}
+			if (s1==1)
+			{
+				if(c1==0)
+				red_p2=0;
+				if (c1 >= 1 && c1 <= 255&&c1!=76&&c1!=71&&c1!=66)
+				{
+					red_p2 = c1*4-1;
+					red_br2=red_p2;
+				}
+				else
+				s1=0;
+			}
+
+			if(USART3->DR == 'G')
+			{
+				brightness2=100;
+				green_p2 = 0;
+				k1 = 1;
+			}
+			if (k1==1)
+			{
+				if(c1<1)
+				green_p2=0;
+				if (c1 >= 1 && c1 <= 255&&c1!=76&&c1!=82&&c1!=66)
+				{
+					green_p2 = c1*4-1;
+					green_br2=green_p2;
+				}
+				else
+				k1=0;
+			}
+
+			if(USART3->DR == 'B')
+			{
+				brightness2=100;
+				blue_p2 = 0;
+				m1 = 1;
+			}
+			if (m1==1)
+			{
+				if(c1<1)
+				blue_p2=0;
+				if (c1 >= 1 && c1 <= 255&&c1!=76&&c1!=82&&c1!=71)
+				{
+					blue_p2 = c1*4-1;
+					blue_br2=blue_p2;
+				}
+				else
+				m1=0;
+			}
+			// RGB - RING_2
+			rgb2(red_p2, green_p2,blue_p2);
+
+			if(brightness2)
+			{
+				if(red_br2==327)
+				{
+					red_br2=0;
+				}
+				if(green_br2==283)
+				{
+					green_br2=0;
+				}
+				if(blue_br2==263)
+				{
+					blue_br2=0;
+				}
+				int i=0;
+
+				for(i=8;i<16;i++)
+				{
+					diode[i].red=(red_br2* brightness2) >> 8;
+					diode[i].green=(green_br2* brightness2) >> 8;
+					diode[i].blue=(blue_br2* brightness2) >> 8;
+				}
+			}
+			// KOLORY - RING_2
+			if(USART3->DR == 'g')
+			{
+				red_br2=135;
+				green_br2=31;
+				blue_br2=0;
+				brightness2=100;
+				rgb2(red_br2, green_br2,blue_br2);
+			}
+			if(USART3->DR == 'h')
+			{
+				red_br2=195;
+				green_br2=111;
+				blue_br2=0;
+				brightness2=100;
+				rgb2(red_br2, green_br2,blue_br2);
+			}
+			if(USART3->DR == 'i')
+			{
+				red_br2=24;
+				green_br2=100;
+				blue_br2=0;
+				brightness2=100;
+				rgb2(red_br2, green_br2,blue_br2);
+			}
+			if(USART3->DR == 'j')
+			{
+				red_br2=0;
+				green_br2=243;
+				blue_br2=75;
+				brightness2=100;
+				rgb2(red_br2, green_br2,blue_br2);
+			}
+			if(USART3->DR == 'k')
+			{
+				red_br2=255;
+				green_br2=0;
+				blue_br2=51;
+				brightness2=100;
+				rgb2(red_br2, green_br2,blue_br2);
+			}
+			if(USART3->DR == 'l')
+			{
+				red_br2=255;
+				green_br2=0;
+				blue_br2=255;
+				brightness2=100;
+				rgb2(red_br2, green_br2,blue_br2);
+			}
+			if(USART3->DR == 'm')
+			{
+				red_br2=19;
+				green_br2=0;
+				blue_br2=207;
+				brightness2=100;
+				rgb2(red_br2, green_br2,blue_br2);
+			}
+
+			// SEKWENCJE - RING_2
+			if(USART3->DR == 'a')
+			{
+				red_p2=0;
+				green_p2=0;
+				blue_p2=0;
+				zmienna_usart12=1;
+			}
+			if(USART3->DR == 'b')
+			{
+				red_p2=0;
+				green_p2=0;
+				blue_p2=0;
+				zmienna_usart22=1;
+			}
+			if(USART3->DR == 'c')
+			{
+				red_p2=0;
+				green_p2=0;
+				blue_p2=0;
+				zmienna_usart32=1;
+			}
+			if(USART3->DR == 'd')
+			{
+				red_p2=0;
+				green_p2=0;
+				blue_p2=0;
+				zmienna_usart42=1;
+			}
+			if(USART3->DR == 'e')
+			{
+				red_p2=0;
+				green_p2=0;
+				blue_p2=0;
+				zmienna_usart52=1;
+			}
+			if(USART3->DR == 'f')
+			{
+				red_p2=0;
+				green_p2=0;
+				blue_p2=0;
+				zmienna_usart62=1;
+			}
+			// TURN OFF
+			if(USART3->DR == 'O')
+			{
+				turn_led2=1;
+				clear_app(8,16);
+				red_br2=0;
+				green_br2=0;
+				blue_br2=0;
+				red_p2=0;
+				green_p2=0;
+				blue_p2=0;
+			}
+		}
+	}
+}
+
+int main(void)
+{
+	SystemInit();
+	rcc();
+	gpio();
+	usart();
+	init_systick();	//delay
+	ws2812_init();	//LED
+
+	while(1)
+	{
+		if(zmienna_usart1==1)
+		{
+			seq1(0,8);
+			if(turn_led==1)
+			{
+				zmienna_usart1=0;
+				clear_app(0,8);
+				turn_led=0;
+			}
+		}
+		if(zmienna_usart2==1)
+		{
+			seq2(0,8);
+			if(turn_led==1)
+			{
+				zmienna_usart2=0;
+				clear_app(0,8);
+				turn_led=0;
+			}
+		}
+		if(zmienna_usart3==1)
+		{
+			seq3(0,8);
+			if(turn_led==1)
+			{
+				zmienna_usart3=0;
+				clear_app(0,8);
+				turn_led=0;
+			}
+		}
+		if(zmienna_usart4==1)
+		{
+			seq10(0,8);
+			if(turn_led==1)
+			{
+				zmienna_usart4=0;
+				clear_app(0,8);
+				turn_led=0;
+			}
+		}
+		if(zmienna_usart5==1)
+		{
+			seq7(0,8);
+			if(turn_led==1)
+			{
+				zmienna_usart5=0;
+				clear_app(0,8);
+				turn_led=0;
+			}
+		}
+		if(zmienna_usart6==1)
+		{
+			seq6(0,8);
+			if(turn_led==1)
+			{
+				zmienna_usart6=0;
+				clear_app(0,8);
+				turn_led=0;
+			}
+		}
+
+		//////////////////RING2///////////////////////////
+		if(zmienna_usart12==1)
+		{
+			seq1(8,16);
+			if(turn_led2==1)
+			{
+				zmienna_usart12=0;
+				clear_app(8,16);
+				turn_led2=0;
+			}
+		}
+		if(zmienna_usart22==1)
+		{
+			seq2(8,16);
+			if(turn_led2==1)
+			{
+				zmienna_usart22=0;
+				clear_app(8,16);
+				turn_led2=0;
+			}
+		}
+		if(zmienna_usart32==1)
+		{
+			seq3(8,16);
+			if(turn_led2==1)
+			{
+				zmienna_usart32=0;
+				clear_app(8,16);
+				turn_led2=0;
+			}
+		}
+		if(zmienna_usart42==1)
+		{
+			seq9(8,16);
+			if(turn_led2==1)
+			{
+				zmienna_usart42=0;
+				clear_app(8,16);
+				turn_led2=0;
+			}
+		}
+		if(zmienna_usart52==1)
+		{
+			seq8(8,16);
+			if(turn_led2==1)
+			{
+				zmienna_usart52=0;
+				clear_app(8,16);
+				turn_led2=0;
+			}
+		}
+		if(zmienna_usart62==1)
+		{
+			seq6(8,16);
+			if(turn_led2==1)
+			{
+				zmienna_usart62=0;
+				clear_app(8,16);
+				turn_led2=0;
+			}
+		}
+	}
 }
